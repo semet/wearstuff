8c41d9406e08a15f8722150052689579b4d25388
Semet <hamdanilombok@gmail.com>
2023-02-11 09:47:07 +0700

    add gender and photo

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\GenderEnum;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -24,6 +25,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'gender' => fake()->randomElement(GenderEnum::cases()),
+            'photo' => fake()->imageUrl(400, 400, true)
         ];
     }
 
