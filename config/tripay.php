c74f95e6a677bc4a373a160fd76e1fdc2edaae1b
Semet <hamdanilombok@gmail.com>
2023-02-12 04:52:57 +0700

    add tripay config

---
+++ config/tripay.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'merchant_code' => env('TRIPAY_MERCHANT_CODE'),
+    'merchant_name' => env('TRIPAY_MERCHANT_NAME'),
+    'api_key' => env('TRIPAY_API_KEY'),
+    'private_key' => env('TRIPAY_PRIVATE_KEY')
+];
