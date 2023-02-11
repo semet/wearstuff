5626f2d298e376806a63bb63c3c0f7f3520e187d
Semet <hamdanilombok@gmail.com>
2023-02-12 04:55:33 +0700

    add midtrans callback url

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Landing\MidtransController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/midtrans/success/callback', [MidtransController::class, 'paymentSuccess'])->name('midtrans.callback');
+Route::post('/midtrans/pending/callback', [MidtransController::class, 'paymentPending'])->name('midtrans.callback');
