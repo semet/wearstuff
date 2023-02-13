97586734a5f6a0cae6842b8bd69272835eccb986
Semet <hamdanilombok@gmail.com>
2023-02-13 09:00:33 +0700

    update menu

--- resources/views/components/partials/user-menu.blade.php
+++ resources/views/components/partials/user-menu.blade.php
@@ -13,17 +13,18 @@
             class="dropdown-menu dd-menu dropdown-menu-end bg-white shadow rounded border-0 mt-3 py-3"
             style="width: 200px"
         >
-            <a class="dropdown-item text-dark" href="#"
-                ><i class="uil uil-user align-middle me-1"></i> Account</a
-            >
-            <a class="dropdown-item text-dark" href="#"
-                ><i class="uil uil-clipboard-notes align-middle me-1"></i>
-                Order History</a
-            >
-            <a class="dropdown-item text-dark" href="#"
-                ><i class="uil uil-arrow-circle-down align-middle me-1"></i>
-                Download</a
-            >
+            <a class="dropdown-item text-dark" href="{{ route('account', ['tab' => 'dashboard']) }}">
+                <i class="uil uil-user align-middle me-1"></i> Account
+            </a>
+            <a class="dropdown-item text-dark" href="{{ route('account', ['tab' => 'order']) }}">
+                <i class="uil uil-clipboard-notes align-middle me-1"></i> History Order
+            </a>
+            <a class="dropdown-item text-dark" href="{{ route('account', ['tab' => 'address']) }}">
+                <i class="uil uil-arrow-circle-down align-middle me-1"></i>Alamat
+            </a>
+            <a class="dropdown-item text-dark" href="{{ route('account', ['tab' => 'setting']) }}">
+                <i class="uil uil-arrow-circle-down align-middle me-1"></i>Setting
+            </a>
             <div class="dropdown-divider my-3 border-top"></div>
             <a class="dropdown-item text-dark" href="{{ route('logout') }}"><i class="uil uil-sign-out-alt align-middle me-1"></i>
                 Logout
