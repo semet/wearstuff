6a3e93b8539c8c1057d4cebb89c64997e47798c2
Semet <hamdanilombok@gmail.com>
2023-02-13 09:01:37 +0700

    clean it up

--- resources/views/components/shared/product-review-form.blade.php
+++ resources/views/components/shared/product-review-form.blade.php
@@ -1,35 +1,16 @@
 <div class="col-lg-6 mt-4 mt-lg-0 pt-2 pt-lg-0">
-    <form class="ms-lg-4">
+    <form class="ms-lg-4 needs-validation" action="{{ route('review.store', $productId) }}" method="POST" novalidate>
+        @csrf
         <div class="row">
             <div class="col-12">
                 <h5>Tambahkan review anda:</h5>
             </div>
             <div class="col-md-12 mt-3">
                 <div class="mb-3">
-                    <label class="form-label">Your Review:</label>
+                    <label class="form-label" for="message">Your Review:</label>
                     <div class="form-icon position-relative">
                         <i data-feather="message-circle" class="fea icon-sm icons"></i>
-                        <textarea id="message" placeholder="Your Comment" rows="5" name="message" class="form-control ps-5" required=""></textarea>
-                    </div>
-                </div>
-            </div><!--end col-->
-
-            <div class="col-lg-6">
-                <div class="mb-3">
-                    <label class="form-label">Name <span class="text-danger">*</span></label>
-                    <div class="form-icon position-relative">
-                        <i data-feather="user" class="fea icon-sm icons"></i>
-                        <input id="name" name="name" type="text" placeholder="Name" class="form-control ps-5" required="">
-                    </div>
-                </div>
-            </div><!--end col-->
-
-            <div class="col-lg-6">
-                <div class="mb-3">
-                    <label class="form-label">Your Email <span class="text-danger">*</span></label>
-                    <div class="form-icon position-relative">
-                        <i data-feather="mail" class="fea icon-sm icons"></i>
-                        <input id="email" type="email" placeholder="Email" name="email" class="form-control ps-5" required="">
+                        <textarea id="message" placeholder="Your Comment" rows="5" name="message" class="form-control ps-5" required></textarea>
                     </div>
                 </div>
             </div><!--end col-->
