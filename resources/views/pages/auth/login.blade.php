1b27f89698ef0d45197d19527ce8ba556b09ea01
Semet <hamdanilombok@gmail.com>
2023-02-13 09:02:16 +0700

    update route

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -58,7 +58,7 @@
                                                 </div>
                                             </div>
                                             <p class="forgot-pass mb-0">
-                                                <a href="{{ route('reset.password') }}" class="text-dark fw-bold">Forgot password ?</a>
+                                                <a href="{{ route('password.reset') }}" class="text-dark fw-bold">Forgot password ?</a>
                                             </p>
                                         </div>
                                     </div><!--end col-->
@@ -87,7 +87,7 @@
                                     </div><!--end col-->
 
                                     <div class="col-12 text-center">
-                                        <p class="mb-0 mt-3"><small class="text-dark me-2">Don't have an account ?</small> <a href="{{ route('register') }}" class="text-dark fw-bold">Sign Up</a></p>
+                                        <p class="mb-0 mt-3"><small class="text-dark me-2">Don't have an account ?</small> <a href="{{ route('register.show') }}" class="text-dark fw-bold">Sign Up</a></p>
                                     </div><!--end col-->
                                 </div><!--end row-->
                             </form>
