e1f54c60ea612536fb7533e74c42cebc9a55b1a6
Semet <hamdanilombok@gmail.com>
2023-02-13 09:02:35 +0700

    a little clean up

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -13,45 +13,92 @@
                 <div class="col-lg-5 col-md-6">
                     <div class="card shadow rounded border-0">
                         <div class="card-body">
-                            <h4 class="card-title text-center">Signup</h4>
-                            <form class="login-form mt-4">
+                            <h4 class="card-title text-center">Registrasi</h4>
+                            <form class="login-form mt-4" method="POST" action="{{ route('register.post') }}">
+                                @csrf
                                 <div class="row">
-                                    <div class="col-md-6">
+                                    <div class="col-md-12">
                                         <div class="mb-3">
-                                            <label class="form-label">First name <span class="text-danger">*</span></label>
-                                            <div class="form-icon position-relative">
-                                                <i data-feather="user" class="fea icon-sm icons"></i>
-                                                <input type="text" class="form-control ps-5" placeholder="First Name" name="s" required="">
-                                            </div>
-                                        </div>
-                                    </div><!--end col-->
-
-                                    <div class="col-md-6">
-                                        <div class="mb-3">
-                                            <label class="form-label">Last name <span class="text-danger">*</span></label>
+                                            <label class="form-label" for="name">Nama Lengkap <span class="text-danger">*</span></label>
                                             <div class="form-icon position-relative">
                                                 <i data-feather="user-check" class="fea icon-sm icons"></i>
-                                                <input type="text" class="form-control ps-5" placeholder="Last Name" name="s" required="">
+                                                <input type="text" class="form-control ps-5 @error('name') is-invalid @enderror" placeholder="Nama Lengkap" name="name" id="name" required="Please">
+                                                @error('name')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
                                             </div>
                                         </div>
                                     </div><!--end col-->
 
                                     <div class="col-md-12">
                                         <div class="mb-3">
-                                            <label class="form-label">Your Email <span class="text-danger">*</span></label>
+                                            <label class="form-label" for="phone">No. Telepon <span class="text-danger">*</span></label>
+                                            <div class="form-icon position-relative">
+                                                <i data-feather="phone" class="fea icon-sm icons"></i>
+                                                <input type="text" class="form-control ps-5 @error('phone') is-invalid @enderror" placeholder="Nomor Telepon" name="phone" id="phone" required="">
+                                                @error('phone')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                            </div>
+                                        </div>
+                                    </div><!--end col-->
+
+                                    <div class="col-md-12">
+                                        <div class="mb-3">
+                                            <label class="form-label" for="email">Email <span class="text-danger">*</span></label>
                                             <div class="form-icon position-relative">
                                                 <i data-feather="mail" class="fea icon-sm icons"></i>
-                                                <input type="email" class="form-control ps-5" placeholder="Email" name="email" required="">
+                                                <input type="email" class="form-control ps-5 @error('email') is-invalid @enderror" placeholder="Email" name="email" id="email" required="">
+                                                @error('email')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                            </div>
+                                        </div>
+                                    </div><!--end col-->
+                                    <div class="col-md-12">
+                                        <div class="mb-3">
+                                            <label class="form-label" for="gender">Jenis Kelamin <span class="text-danger">*</span></label>
+                                            <select name="gender" id="gender" class="form-select @error('gender') is-invalid @enderror">
+                                                <option value="">--Pilih Jenis Kelamin--</option>
+                                                @foreach (\App\Enums\GenderEnum::cases() as $gender)
+                                                    <option value="{{ $gender->value }}">{{ $gender->value }}</option>
+                                                @endforeach
+                                            </select>
+                                            @error('gender')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div>
+                                    </div><!--end col-->
+
+                                    <div class="col-md-12">
+                                        <div class="mb-3">
+                                            <label class="form-label" for="password">Password <span class="text-danger">*</span></label>
+                                            <div class="form-icon position-relative">
+                                                <i data-feather="key" class="fea icon-sm icons"></i>
+                                                <input type="password" class="form-control ps-5 @error('password') is-invalid @enderror" placeholder="Password" name="password" id="password" required="">
+                                                @error('password')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
                                             </div>
                                         </div>
                                     </div><!--end col-->
 
                                     <div class="col-md-12">
                                         <div class="mb-3">
-                                            <label class="form-label">Password <span class="text-danger">*</span></label>
+                                            <label class="form-label" for="passwordConfirmation">Ulang Password <span class="text-danger">*</span></label>
                                             <div class="form-icon position-relative">
                                                 <i data-feather="key" class="fea icon-sm icons"></i>
-                                                <input type="password" class="form-control ps-5" placeholder="Password" required="">
+                                                <input type="password" class="form-control ps-5" placeholder="Password" name="password_confirmation" id="passwordConfirmation" required="">
                                             </div>
                                         </div>
                                     </div><!--end col-->
@@ -59,20 +106,20 @@
                                     <div class="col-md-12">
                                         <div class="mb-3">
                                             <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                                <label class="form-check-label" for="flexCheckDefault">I Accept <a href="#" class="text-primary">Terms And Condition</a></label>
+                                                <input class="form-check-input @error('terms_and_condition') is-invalid @enderror" type="checkbox" value="accept" id="flexCheckDefault" name="terms_and_condition">
+                                                <label class="form-check-label" for="flexCheckDefault">Saya setuju <a href="#" class="text-primary">syarat dan ketentuan</a></label>
                                             </div>
                                         </div>
                                     </div><!--end col-->
 
                                     <div class="col-md-12">
                                         <div class="d-grid">
-                                            <button class="btn btn-primary">Register</button>
+                                            <button class="btn btn-primary" type="submit">Register</button>
                                         </div>
                                     </div><!--end col-->
 
                                     <div class="col-lg-12 mt-4 text-center">
-                                        <h6>Or Signup With</h6>
+                                        <h6>atau Daftar dengan</h6>
                                         <div class="row">
                                             <div class="col-6 mt-3">
                                                 <div class="d-grid">
@@ -89,7 +136,10 @@
                                     </div><!--end col-->
 
                                     <div class="mx-auto">
-                                        <p class="mb-0 mt-3"><small class="text-dark me-2">Already have an account ?</small> <a href="{{ route('login') }}" class="text-dark fw-bold">Sign in</a></p>
+                                        <p class="mb-0 mt-3">
+                                            <small class="text-dark me-2">Sudah punya akun ?</small>
+                                            <a href="{{ route('login.show') }}" class="text-dark fw-bold">Login</a>
+                                        </p>
                                     </div>
                                 </div><!--end row-->
                             </form>
