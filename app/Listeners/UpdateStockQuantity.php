a9d25cd31682303dc89fd2092f140a37b175c258
Semet <hamdanilombok@gmail.com>
2023-02-13 08:54:56 +0700

    listeners to process updating stock quantity

--- app/Listeners/UpdateStockQuantity.php
+++ app/Listeners/UpdateStockQuantity.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\PaymentSuccessful;
+use App\Events\StockRunningLow;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Product;
@@ -30,7 +31,13 @@ class UpdateStockQuantity
     public function handle(PaymentSuccessful $event)
     {
         $event->order->items->each(function (OrderItem $item) use ($event) {
+            //reduce the stock amount based on ordered quantity
             $item->product()->decrement('quantity', $item->quantity);
+            //check if stock amount <= 5, if true, notify admin!
+            if ($item->product->quantity <= 3) {
+                StockRunningLow::dispatch($item->product);
+            }
+            //add the sold item into the database
             $item->product()->each(function (Product $product) use ($event, $item) {
                 $product->solds()->create([
                     'user_id' => $event->order->user->id,
