a14361d01df3a33fca50e9c92436bd0f41b8c427
Semet <hamdanilombok@gmail.com>
2023-02-13 08:53:12 +0700

    add order cancelled and order expired events

--- app/Http/Controllers/Landing/MidtransController.php
+++ app/Http/Controllers/Landing/MidtransController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Landing;
 
 use App\Enums\PaymentStatusEnum;
+use App\Events\OrderCancelled;
+use App\Events\OrderExpired;
 use App\Events\PaymentSuccessful;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
@@ -18,16 +20,25 @@ class MidtransController extends Controller
         //SHA512(order_id+status_code+gross_amount+ServerKey)
         $hashed = hash('SHA512', $request->order_id . $request->status_code . $request->gross_amount . $serverKey);
         $order = Order::where('number', $request->order_id)->first();
-        if ($hashed == $request->signature_key) {
-            if ($request->transaction_status == 'capture' || $request->transaction_status == 'settlement') {
-                $order->payment_status = PaymentStatusEnum::PAID;
-                $order->save();
+        //check if order is Expire ?
+        if ($order->payment_status == PaymentStatusEnum::EXPIRE->value) {
+            OrderExpired::dispatch($order);
+            //Check if order has been cancelled
+        } elseif ($order->payment_status == PaymentStatusEnum::CANCELED->value) {
+            OrderCancelled::dispatch($order);
+        } elseif ($order->payment_status == PaymentStatusEnum::PENDING->value) {
+            //check Signature Key
+            if ($hashed == $request->signature_key) {
+                if ($request->transaction_status == 'capture' || $request->transaction_status == 'settlement') {
+                    $order->payment_status = PaymentStatusEnum::PAID;
+                    $order->save();
 
-                PaymentSuccessful::dispatch($order);
+                    PaymentSuccessful::dispatch($order);
+                }
+            } else {
+                Log::error('Invalid signature key');
+                throw new Exception('Invalid signature key');
             }
-        } else {
-            Log::error('Invalid signature key');
-            throw new Exception('Invalid signature key');
         }
     }
 }
