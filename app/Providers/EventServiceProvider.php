0788b67b1967fa9511265ae3386426c2d65cc0f1
Semet <hamdanilombok@gmail.com>
2023-02-14 22:43:08 +0700

    add extra event listeners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,17 +4,21 @@ namespace App\Providers;
 
 use App\Events\OrderCancelled;
 use App\Events\OrderExpired;
+use App\Events\OrderPlaced;
 use App\Events\PasswordResetLinkRequested;
 use App\Events\PaymentSuccessful;
 use App\Events\StockRunningLow;
 use App\Events\UserCreated;
 use App\Listeners\NotifyAdmin;
+use App\Listeners\RestoreStockQuantity;
+use App\Listeners\SayThankyouToCustomer;
 use App\Listeners\SendCancelledOrderNotification;
 use App\Listeners\SendEmailVerification;
 use App\Listeners\SendExpiredOrderNotification;
 use App\Listeners\SendInvoiceToCustomer;
 use App\Listeners\SendLowStockNotification;
 use App\Listeners\SendPasswordResetLink;
+use App\Listeners\UpdateCustomerPoint;
 use App\Listeners\UpdateShoppingCart;
 use App\Listeners\UpdateStockQuantity;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -31,24 +35,34 @@ class EventServiceProvider extends ServiceProvider
         UserCreated::class => [
             SendEmailVerification::class,
         ],
+        OrderPlaced::class => [
+            SayThankyouToCustomer::class,
+            //update cart
+            UpdateShoppingCart::class
+        ],
         PaymentSuccessful::class => [
             //send invoice to customer;
             SendInvoiceToCustomer::class,
+            //Update the point
+            UpdateCustomerPoint::class,
             //send notification to Admin;
             NotifyAdmin::class,
             //Update stock
             UpdateStockQuantity::class,
-            //update cart
-            UpdateShoppingCart::class
+
         ],
         StockRunningLow::class => [
             SendLowStockNotification::class
         ],
         OrderExpired::class => [
-            SendExpiredOrderNotification::class
+            SendExpiredOrderNotification::class,
+            //restore stock quantity
+            RestoreStockQuantity::class
         ],
         OrderCancelled::class => [
-            SendCancelledOrderNotification::class
+            SendCancelledOrderNotification::class,
+            //restore stock quantity
+            RestoreStockQuantity::class
         ],
         PasswordResetLinkRequested::class => [
             SendPasswordResetLink::class
