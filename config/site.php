5955ae2a98d42334289aab05350856fb3cc72e6c
Semet <hamdanilombok@gmail.com>
2023-02-16 16:56:50 +0700

    add more site config

--- config/site.php
+++ config/site.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-    'minimum_expense' => 250000
+    'minimum_expense' => 250000,
+
+    'address' => [
+        'city' => 'Lombok Tengah',
+        'province' => 'Nusa Tenggara Barat (NTB)',
+        'line' => 'Desa Sengkerang, Kecamatan Praya Timur'
+    ],
+    'phone' => '087736690307',
+    'email' => 'support@wearstuff.com'
 ];
